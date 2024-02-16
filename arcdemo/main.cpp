a769142b3590d7e23df9bd68c40253133df74aa4
aranyp20 <aranypeter20@gmail.com>
2024-02-16 10:27:27 +0100

    glfw window

---
+++ arcdemo/main.cpp
@@ -0,0 +1,27 @@
+#include <GLFW/glfw3.h>
+
+int main() {
+
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "arcdemo app", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
